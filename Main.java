2023cef48d114dc071d414d82e0d8c263733384a
victoria-vepreve <victoriav88@nycstudents.net>
2024-10-07 09:19:30 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -13,23 +13,37 @@ public class Main
         // info = game.getScore();
         // System.out.println(info);
 
-
         // Below is the test case from the AP task.
         String info;
         Scoreboard game = new Scoreboard("Red", "Blue");
         info = game.getScore();
+        System.out.println(info);
         game.recordPlay(1);
         info = game.getScore();
+        System.out.println(info);
+        game.recordPlay(0);
+        info = game.getScore();
+        info = game.getScore();
+        System.out.println(info);
+        System.out.println(info);
+        game.recordPlay(3);
+        info = game.getScore();
+        System.out.println(info);
         game.recordPlay(1);
         game.recordPlay(0);
         info = game.getScore();
+        System.out.println(info);
         game.recordPlay(0);
         game.recordPlay(4);
         game.recordPlay(0);
         info = game.getScore();
+        System.out.println(info);
         Scoreboard match = new Scoreboard("Lions", "Tigers");
         info = match.getScore();
+        System.out.println(info);
         info = game.getScore();
+        System.out.println(info);
+
         
     }
 }
